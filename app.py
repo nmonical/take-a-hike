72aa9a7bf184a570455f8db922c5ff3871b3ddec
nmonical <124633769+nmonical@users.noreply.github.com>
2024-12-27 21:39:43 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.metrics import pairwise_distances
+
+trails=pickle.load('trails.pkl', 'rb')
+similarity=pickle.load('all_features.pkl', 'rb')
+trail_list=trails['name'].values
+
+st.header("USA Trail Recommendations")
+
+selected_trail = st.selectbox("Select a trail:", trail_list)
+
+import streamlit.components.v1 as components
+
+def get_similar(trail, n=5):
+
+#idx: target item's index
+
+    # 1. compute distance
+    target_feature = similarity[trails['name']==trail.index[0]].reshape(1, -1)
+    couple_dist = pairwise_distances(similarity,
+                                     target_feature, metric='cosine')
+    # 2. get similar dataframe: no need to filter out the first
+    # because the first won't be the unseen url
+    indices = list(
+        map(lambda x: x.item(), np.argsort(couple_dist.ravel())))
+    # similar_score
+    cosine_similarity = 1 - couple_dist[indices].ravel()
+
+    df_sim_all = pd.DataFrame(
+        {"tfidf_index": indices, "similar_score": cosine_similarity})
+    
+    df_sim = df_sim_all[1:n+1]
+    return trails[['name','state_name']].iloc[df_sim['tfidf_index']]
+
+if st.button('Show Hikes'):
+    hike_name, hike_state = get_similar(selected_trail, n=5)['name'].tolist(), get_similar(selected_trail, n=5)['state'].tolist()
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(hike_name[0])
+        st.text(hike_state[0])
+    with col2:
+        st.text(hike_name[1])
+        st.text(hike_state[1])
+    with col3:
+        st.text(hike_name[1])
+        st.text(hike_state[1])
+    with col4:
+        st.text(hike_name[1])
+        st.text(hike_state[1])
+    with col5:
+        st.text(hike_name[1])
+        st.text(hike_state[1])
