ba42c957646b6ce0a14b8335d83675b2d03e307b
nmonical <124633769+nmonical@users.noreply.github.com>
2024-12-28 16:59:10 -0800

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def get_similar(trail, n=5):
     return trails[['name','state_name']].iloc[df_sim['tfidf_index']]
 
 if st.button('Show Hikes'):
-    hike_name, hike_state, slug = get_similar(selected_trail, n=5)['name'].tolist(), get_similar(selected_trail, n=5)['state_name'].tolist(),
+    hike_name, hike_state, slug, area = get_similar(selected_trail, n=5)['name'].tolist(), get_similar(selected_trail, n=5)['state_name'].tolist(),
     get_similar(selected_trail, n=5)['slug'].tolist(), get_similar(selected_trail, n=5)['area_name'].tolist()
     col0, col1, col2, col3, col4, col5 = st.columns(6)
     with col0:
