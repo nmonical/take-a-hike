2ed17dfff164575062defa9f45d6b18764081c8d
nmonical <124633769+nmonical@users.noreply.github.com>
2024-12-29 17:55:17 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ similarity=pickle.load(open('all_features.pkl', 'rb'))
 trail_list=trails['name_area'].values
 area_list=trails['area'].unique()
 
-trails['elevation_gain']=trails['elevation_gain']*3.28084
+trails['elevation_gain']=round(trails['elevation_gain']*3.28084/1000,1)
 trails['length']=trails['length']*0.621371
 
 st.image('yosemite.jpg')
